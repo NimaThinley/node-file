3841893a3afb5d3d36c27ee569123044c74a4b06
NimaThinley <155145985+NimaThinley@users.noreply.github.com>
2024-02-01 15:30:46 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+// const port = 3000;
+const PORT = process.env.PORT;
+const hendle = (FileName, statustCode, req, res) => {
+    fs.readFile(FileName, "utf-8",
+    (err, data) => {
+        if (err){
+            console.log(err);
+        }else{
+            res.writeHead (200 , {
+                "content-type" : "text/html"
+            });
+            res.write(data);
+        }
+    }
+    )
+}
+const server = http.createServer((req, res) => {
+   if(req.url === "/"){
+  hendle("index.html", 200, req, res);
+   }else if (req.url === "/about"){
+    hendle("about.html", 200, req, res);
+   }
+   else if (req.url === "/contact"){
+    hendle("contact.html", 200, req, res);
+   }
+   else{
+    hendle("404.html", 200, req, res);
+}
+});
+server.listen(port, () => {
+    console.log(`server is running`)
+});
+
+
+
+
+
+
+
+
+
+
